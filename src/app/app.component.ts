5c5c5c23eae05f5841a4306724cdb475fb52c807
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-12-21 12:39:42 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,41 @@ export class AppComponent {
     title: ''
   };
 
+  /*
+      debug('start all data loading', queryParam);
+      debug('finish all data loading', queryParam);
+
+  */
+
+
+  /*
+  debug('dataTables processing');
+  debug('queryResult processing');
+  */
+
+
+  /*
+          debug('queryResult processing', queryParam);
+        debug('result ordering', queryParam);
+  */
+
+
+  /*
+          debug('result ordering', queryParam);
+        debug('final result is ready', queryParam);
+
+  */
+
+  /*
+      debug(`start reading "${filePath}"`);
+      debug(`finish reading "${filePath}"`);
+  */
+
+  /*
+      debug('resources list by query', {queryParam, resources});
+  */
+
+
   openFile(event) {
     let input = event.target;
     for (var index = 0; index < input.files.length; index++) {
@@ -26,8 +61,35 @@ export class AppComponent {
     };
   }
 
+  setCurrentTest(test) {
+    this.currentTest = test;
+    // this.calculateOptions();
+  }
+
   details(record) {
     this.detailsContent = JSON.stringify(record.attachment, null, 4).replace(/\n/g, '<br>').replace(/\s/g, '&nbsp;');
     this.detailsMode = true;
+  }
+
+  calculateOptions() {
+    console.log(this.getOptionsByStartAndFinish('start all data loading', 'finish all data loading'));
+  }
+
+  private getOptionsByStartAndFinish(startMessage: string, finishMessage: string): any {
+    const result = {};
+
+    for (const item of this.currentTest.diagnostic) {
+      if (item.message === startMessage) {
+        const hash = JSON.stringify(item.attachment);
+        result[hash] = new Date(item.time).getTime();
+      }
+
+      if (item.message === finishMessage) {
+        const hash = JSON.stringify(item.attachment);
+        result[hash] = (new Date(item.time).getTime() - result[hash]);
+      }
+    }
+
+    return result;
   }
 }
